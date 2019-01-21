96410a8b9088841d7c3e0ac03b7e3e375558c0dd
jhenzi <jhenzi@cisco.com>
2019-01-21 10:26:04 -0500

    Added ETL pipline files
    Created directory "data" to store ETL pipline files
    
    Add file process_data.py : python script toextract the message and category data from flat files, transform them into a single table, and save table to SQLite database
    
    Added flat files disaster_messages.csv and disaster_categories.csv : sample data files for testing.

---
+++ data/process_data.py
@@ -0,0 +1,58 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import sqlite3
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return messages.merge(categories, on = 'id')
+    
+
+
+def clean_data(df):
+    categories = df.categories.str.split(";",expand=True)
+    row = categories.iloc[[0]].squeeze()
+    categories.columns = pd.Series(map(lambda i : i[:-2],row))
+    
+    for column in categories: 
+        categories[column] = pd.to_numeric(categories[column].str[-1:])
+        
+    df.drop("categories",axis=1,inplace=True)
+    df = pd.concat([df,categories],axis=1) 
+    return df.drop_duplicates()
+
+
+def save_data(df, database_filename):
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('Message_Categories', engine, if_exists='replace', index=False) 
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
